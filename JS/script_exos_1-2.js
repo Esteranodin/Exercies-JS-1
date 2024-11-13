567a553ba89db1dfc871db889a49abbb4dba2ded
esteranodin <esteranodin@hotmail.fr>
2024-11-13 16:39:40 +0100

    exos 3 & 4

--- JS/script_exos_1-2.js
+++ JS/script_exos_1-2.js
@@ -1,4 +1,4 @@
-// EXO 1
+//EXO 1
 // let boite = prompt('test de boite de dialogue');
 
 // EXO 2
@@ -8,3 +8,12 @@
 // window.alert ('Nom : ' + lastName + '\n' + 'Prénom : ' + firstName + '\n' + 'Ville : ' + city) 
 
 //EXO 3
+// let prenom = prompt('Tapez votre prénom');
+// window.alert ('Bonjour, ' + prenom);
+
+// Exo 4
+// let lastName = prompt('Tapez votre nom');
+// let firstName = prompt('Tapez votre prenom');
+// let city = prompt('Tapez votre ville');
+// window.alert('Bonjour, ' + lastName + ' ' + firstName + ' ' + 'de ' + city)
+
