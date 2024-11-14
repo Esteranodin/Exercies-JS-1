390414d97e1901d1230fa05c1dc0535e9c4482bc
esteranodin <esteranodin@hotmail.fr>
2024-11-14 11:47:43 +0100

    DONE: exo6

---
+++ JS/script_exo6.js
@@ -0,0 +1,9 @@
+function rest() {
+    const number1 = document.getElementById("firstNumber").value;
+    const number2 = document.getElementById("secondNumber").value;
+   
+    let restdiv = number1 % number2;
+
+    window.alert(restdiv)
+}
+
