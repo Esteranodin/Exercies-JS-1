0587b7e8708cc8d99bf4a5b19c814d2c08a25b0a
esteranodin <esteranodin@hotmail.fr>
2024-11-14 11:26:40 +0100

    DONE: exo5

--- JS/script_exo5.js
+++ JS/script_exo5.js
@@ -1,2 +1,9 @@
-let nombre1 = prompt('Tapez un nombre à virugule');
-let nombre2 = prompt('Tapez un deuxième nombre à virugule');
+let number1 = prompt('Tapez un nombre à virugule');
+let number2 = prompt('Tapez un deuxième nombre à virugule');
+
+
+function multi (nombre1, nombre2){
+    return  Math.trunc(nombre1) * nombre2;   
+}
+
+window.alert(multi(number1, number2));
