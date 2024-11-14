fa8850dd1d73037eeba78ac9b467d272d7263883
esteranodin <esteranodin@hotmail.fr>
2024-11-14 13:37:33 +0100

    DONE: exo8

---
+++ JS/script_exo8.js
@@ -0,0 +1,9 @@
+function major() {
+    const age = document.getElementById("age").value;
+    if (age >= 18) {
+        window.alert("Vous êtes majeur");   
+    }
+    else {
+        window.alert("Vous êtes mineur");
+    };
+}
